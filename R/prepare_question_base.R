6a30574ffcfb1e85ed42d8c7b828dc836d5ae55f
Alaburda <p.alaurda1@gmail.com>
2020-09-24 23:08:38 +0300

    Initial commit

--- R/prepare_question_base.R
+++ R/prepare_question_base.R
@@ -1,6 +1,7 @@
 #' @title FIlter a set of question based on your needs
 #'
-#' @description This function filters a set of questions for further use in a pub quiz slide presentation
+#' @description Filters a set of questions for further use in a pub quiz slide presentation.
+#' The function expects that the supplied data frame contains variables with the following names: description, audio, question_image
 #' @param db database object
 #' @param n number of questions to keep
 #' @param include_text Can the set contain questions with text?
@@ -17,7 +18,8 @@ prepare_question_base <- function(db,
                                   include_audio = TRUE,
                                   include_image = FALSE) {
 
-  db <- filter(db, !(!is.na(description) != include_text & !is.na(audio) != include_audio & !is.na(question_image) != include_image))
-  db_sample <- db[sample(nrow(db),n),]
+  db_filtered <- dplyr::filter(db, !(!is.na(description) != include_text & !is.na(audio) != include_audio & !is.na(question_image) != include_image))
+  db_sample <- db_filtered[sample(nrow(db_filtered),n),]
+  db_sample$question <- glue::glue("*{db_sample$question}*")
   return(db_sample)
 }
